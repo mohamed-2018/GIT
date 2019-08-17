0f3216e4654481c8c8ecb0159ef91a1f9e7131fa
alexmlrepo <52533555+alexmlrepo@users.noreply.github.com>
2019-08-17 20:19:25 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,9 +1,9 @@
-class Test
-{
-
-public static void main(String [] args);
-{
-test code;
-}
-
-}
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+} 
